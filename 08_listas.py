c73624e372921764af62de98dc954adaf6979b1f
João Mateus <80249973+joaomateuus@users.noreply.github.com>
2022-06-10 12:44:33 -0300

    Update 08_listas.py

--- 08_listas.py
+++ 08_listas.py
@@ -9,9 +9,9 @@ numeros_impares = [numeros[1], numeros[2], numeros[3], numeros[5]]
 print(numeros_pares)
 print(numeros_impares)
 
-print(numeros[::2])
-print(numeros[1::2])
-print(numeros[::-1])
+print(numeros[::2]) //pular 2 casa
+print(numeros[1::2]) //pular 2 casas comecando do 1
+print(numeros[::-1]) //ordem inversa
 
 
 
