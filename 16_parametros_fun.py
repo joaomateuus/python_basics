da176ca572caa4b774a277144fac7ad28d6317da
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-06-21 19:35:09 -0300

    changing stuff

--- 16_parametros_fun.py
+++ 16_parametros_fun.py
@@ -1,46 +1,53 @@
-# # Passando multiplos parametros
-# def soma(*args):
-#     return sum(args)
-
-# res = soma(10, 3, 6, 7)
-# print(res)
-
-# # # Se quiser passar lista
+# # # Passando multiplos parametros
 # # def soma(*args):
 # #     return sum(args)
 
-# # numeros = [10, 34, 4, 3, 4]
-# # print(soma(*numeros))
+# # res = soma(10, 3, 6, 7)
+# # print(res)
 
-# def multiplicar(*args) -> float:
-#     n = 1
-#     for a in args:
-#         n *= a
-#         return n
+# # # # Se quiser passar lista
+# # # def soma(*args):
+# # #     return sum(args)
 
-# numeros = [2, 3, 4, 5]
-# print(multiplicar(*numeros))
-# print(multiplicar(3, 4, 5, 6))
+# # # numeros = [10, 34, 4, 3, 4]
+# # # print(soma(*numeros))
 
-# def pessoa(**kwargs):
-#     return kwargs
-# print(pessoa(nome = 'Joao Mateus', idade=20, altura = 1.68))
+# # def multiplicar(*args) -> float:
+# #     n = 1
+# #     for a in args:
+# #         n *= a
+# #         return n
 
-numero1 = input('Digite o n1: ')
-numero2 = input('Digite o n2: ')
-opera = input('Digite a operacao (+,-,*,/):  ')
+# # numeros = [2, 3, 4, 5]
+# # print(multiplicar(*numeros))
+# # print(multiplicar(3, 4, 5, 6))
 
-numeros = {}
-numeros["numero1"] = numero1
-numeros["numero2"] = numero2
-numeros["operacao"] = opera
+# # def pessoa(**kwargs):
+# #     return kwargs
+# # print(pessoa(nome = 'Joao Mateus', idade=20, altura = 1.68))
+
+# numero1 = input('Digite o n1: ')
+# numero2 = input('Digite o n2: ')
+# opera = input('Digite a operacao (+,-,*,/):  ')
+
+# numeros = {}
+# numeros["numero1"] = numero1
+# numeros["numero2"] = numero2
+# numeros["operacao"] = opera
 
 
-def aritimetica(**kwargs):
-    n1 = kwargs.get('numero1', 0)
-    n2 = kwargs.get('numero2', 0)
-    op = kwargs.get('operacao', '+')
+# def aritimetica(**kwargs):
+#     n1 = kwargs.get('numero1', 0)
+#     n2 = kwargs.get('numero2', 0)
+#     op = kwargs.get('operacao', '+')
     
-    return eval(f'{n1} {op} {n2}')
+#     return eval(f'{n1} {op} {n2}')
 
-print(aritimetica(**numeros))
+# print(aritimetica(**numeros))
+
+# Digitar um monte de coisa na calculadora e e fazer o calculo ex:23423+23423-4534534/4
+input_usuario = input('Digite a operacao que deseja fazer:  ')
+def calc_expressao(args):
+    return eval(args)
+print(calc_expressao(input_usuario))
+
